9a115d07b9ff8a23300528cd868e0d26d170eb6d
Ian Bogley <ibogley@uoregon.edu>
2021-02-22 14:12:13 -0800

    added file for final presentation, as well as testing script with mvp/allstar dataframes

---
+++ mvp_allstar_code.R
@@ -0,0 +1,14 @@
+library(pacman)
+p_load(rvest,tidyverse)
+
+mvp <- read_html("https://en.wikipedia.org/wiki/NBA_Most_Valuable_Player_Award") %>%
+  html_nodes(xpath = "//*[@id='mw-content-text']/div[1]/table[4]") %>%
+  html_table() %>%
+  data.frame()
+mvp
+
+allstars <- read_html("https://en.wikipedia.org/wiki/List_of_NBA_All-Stars") %>%
+  html_nodes(xpath = "//*[@id='mw-content-text']/div[1]/table[2]") %>%
+  html_table() %>%
+  data.frame()
+allstars
