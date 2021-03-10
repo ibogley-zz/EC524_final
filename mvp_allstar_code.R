1ded8edeb81d9cd78fb34b670950a25078408917
JonasBowman <78988689+JonasBowman@users.noreply.github.com>
2021-03-10 11:20:43 -0800

    deleted r markdown

--- mvp_allstar_code.R
+++ mvp_allstar_code.R
@@ -14,3 +14,7 @@ allstars <- read_html("https://en.wikipedia.org/wiki/List_of_NBA_All-Stars") %>%
 allstars
 
 
+logit_df%>% filter(allstar>.9, year==2006)%>%ggplot(aes(x=allstar, y= reorder(player,(allstar)),fill=player))+geom_col()+theme_classic(base_size = 12)+scale_fill_viridis_d() +xlab("Prediction") +ylab("Player")+ ggtitle("2006 Allstar Predictions Logistic Regression Model")
+
+
+
